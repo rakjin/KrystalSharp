2cf25f1469f3425a5998743016c28ba63002bdb9
rakjin <grampush@gmail.com>
2013-11-26 09:42:25 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrystalSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
